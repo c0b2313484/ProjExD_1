8cd0dec8d7b3b2bc55f24da217d0ce5b60bbb8ad
C0B23134 <c0b2313484@edu.teu.ac.jp>
2024-06-11 16:32:15 +0900

    7_ワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #1
+    bg_img_2 = pg.transform.flip(bg_img, True, False) #7_1:反転した画像
     kt_img = pg.image.load("fig/3.png") #2_読み込み
     kt_img = pg.transform.flip(kt_img, True, False) #2_左右反転
     tmr = 0
@@ -17,8 +18,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800 #6
+        x = tmr%3200 #6
         screen.blit(bg_img, [-x, 0]) #3
+        screen.blit(bg_img_2, [-x+1600, 0]) #7-1
+        screen.blit(bg_img, [-x+3200, 0]) #7-2
+        screen.blit(bg_img_2, [-x+4800, 0]) #7-2
+
         kt_rct = kt_img.get_rect() #4_こうかとん画像rectを取得する
         kt_rct.center = 300, 200
         screen.blit(kt_img, kt_rct) #kt_img画像をkt_rctに貼る
